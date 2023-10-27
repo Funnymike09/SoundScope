8fc610cc86576e81a3f2ab21afcf3b9f13d5eb11
Funnymike09 <mike.nip@mail.ru>
2023-10-27 08:11:41 +0500

    Samll tutorial and additional polishes

--- SoundPrototype/Assets/scripts/DeeathScript.cs
+++ SoundPrototype/Assets/scripts/DeeathScript.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
-using OpenCover.Framework.Model;
+
 
 public class DeeathScript : MonoBehaviour
 {
